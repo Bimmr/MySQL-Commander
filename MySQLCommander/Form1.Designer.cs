61d3826aa3e60a5bcffe8743d5409247399d0daa
Bimmr <bimmr6696@gmail.com>
2016-10-07 16:01:48 -0400

    Init

---
+++ MySQLCommander/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace MySQLCommander
+{
+    partial class MySQLCommander
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOut = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMySQL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.ofdInput = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectInput = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.btnSelectDatabase = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.cbPassword = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnOut
+            // 
+            this.btnOut.Location = new System.Drawing.Point(121, 348);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(218, 37);
+            this.btnOut.TabIndex = 0;
+            this.btnOut.Text = "Create Ouput File";
+            this.btnOut.UseVisualStyleBackColor = true;
+            this.btnOut.Click += new System.EventHandler(this.btnOut_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtInput.Location = new System.Drawing.Point(12, 38);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(327, 20);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Location(.sql):";
+            // 
+            // txtMySQL
+            // 
+            this.txtMySQL.Location = new System.Drawing.Point(12, 222);
+            this.txtMySQL.Name = "txtMySQL";
+            this.txtMySQL.Size = new System.Drawing.Size(327, 20);
+            this.txtMySQL.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "MySQL Location:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(12, 310);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '•';
+            this.txtPassword.Size = new System.Drawing.Size(327, 20);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // ofdInput
+            // 
+            this.ofdInput.FileName = "openFileDialog1";
+            // 
+            // btnSelectInput
+            // 
+            this.btnSelectInput.Location = new System.Drawing.Point(339, 35);
+            this.btnSelectInput.Name = "btnSelectInput";
+            this.btnSelectInput.Size = new System.Drawing.Size(81, 23);
+            this.btnSelectInput.TabIndex = 10;
+            this.btnSelectInput.Text = "Select File";
+            this.btnSelectInput.UseVisualStyleBackColor = true;
+            this.btnSelectInput.Click += new System.EventHandler(this.btnSelectInput_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Output File Location:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtOutput.Location = new System.Drawing.Point(12, 81);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(327, 20);
+            this.txtOutput.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Database Location:";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(12, 150);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(327, 20);
+            this.txtDatabase.TabIndex = 13;
+            // 
+            // btnSelectDatabase
+            // 
+            this.btnSelectDatabase.Location = new System.Drawing.Point(339, 148);
+            this.btnSelectDatabase.Name = "btnSelectDatabase";
+            this.btnSelectDatabase.Size = new System.Drawing.Size(81, 23);
+            this.btnSelectDatabase.TabIndex = 15;
+            this.btnSelectDatabase.Text = "Select File";
+            this.btnSelectDatabase.UseVisualStyleBackColor = true;
+            this.btnSelectDatabase.Click += new System.EventHandler(this.btnSelectDatabase_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Username";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(12, 271);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(327, 20);
+            this.txtUser.TabIndex = 16;
+            this.txtUser.Text = "root";
+            // 
+            // cbPassword
+            // 
+            this.cbPassword.AutoSize = true;
+            this.cbPassword.Checked = true;
+            this.cbPassword.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbPassword.Location = new System.Drawing.Point(348, 311);
+            this.cbPassword.Name = "cbPassword";
+            this.cbPassword.Size = new System.Drawing.Size(48, 17);
+            this.cbPassword.TabIndex = 18;
+            this.cbPassword.Text = "Hide";
+            this.cbPassword.UseVisualStyleBackColor = true;
+            this.cbPassword.CheckedChanged += new System.EventHandler(this.cbPassword_CheckedChanged);
+            // 
+            // MySQLCommander
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 397);
+            this.Controls.Add(this.cbPassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnSelectDatabase);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnSelectInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMySQL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnOut);
+            this.Name = "MySQLCommander";
+            this.Text = "MySQL Commander";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMySQL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.OpenFileDialog ofdInput;
+        private System.Windows.Forms.Button btnSelectInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Button btnSelectDatabase;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.CheckBox cbPassword;
+    }
+}
+
---
+++ MySQLCommander/Form1.cs
@@ -0,0 +1,200 @@
+﻿/**
+ * 10/7/16
+ * Randy Bimm
+ * 
+ */
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MySQLCommander
+{
+    /// <summary>
+    /// MySQL Commander's main class
+    /// </summary>
+    public partial class MySQLCommander : Form
+    {
+        /// <summary>
+        /// Create a new instance of MySQLCommander
+        /// </summary>
+        public MySQLCommander()
+        {
+            InitializeComponent();
+            getMySQL();
+        }
+        /// <summary>
+        /// Get the latest version of MySQL Server installed on the computer
+        /// </summary>
+        /// <returns></returns>
+        private bool getLatestMySQLServer()
+        {
+            //Check if the folder path exists
+            string path = @"C:\Program Files\MySQL";
+            if (!Directory.Exists(path))
+                return false;
+
+            //Get the latest version of MySQL Server
+            double highest = 0.0;
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                Console.WriteLine(directory);
+                if (directory.Contains("MySQL Server"))
+                {
+                    double t = Double.Parse(directory.Replace(path+"\\MySQL Server", ""));
+                    if (t > highest)
+                        highest = t;
+                }
+            }
+            //If the highest is less than 1, then we didn't find one
+            if (highest < 1)
+                return false;
+
+            //Otherwise update the mysql path with the latest found
+            else
+                txtMySQL.Text = path + "\\MySQL Server " + highest;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Get the MySQL path
+        /// </summary>
+        private void getMySQL()
+        {
+            if (!getLatestMySQLServer() && Directory.Exists(@"C:\xampp\mysql\bin"))
+                txtMySQL.Text = @"C:\xampp\mysql\bin";
+        }
+
+        /// <summary>
+        /// Call the function that creates the out file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOut_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;
+            string output = txtOutput.Text;
+            string database = txtDatabase.Text;
+            string mysql = txtMySQL.Text;
+            string user = txtUser.Text;
+            string password = txtPassword.Text;
+
+            btnOut.Enabled = false;
+            createOutFile(input, output, database, mysql, user, password);
+            btnOut.Enabled = true;
+
+        }
+
+        /// <summary>
+        /// Open a command prompt and create the out file
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        /// <param name="database"></param>
+        /// <param name="mysql"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        private void createOutFile(string input, string output, string database, string mysql, string user, string password)
+        {
+
+            //Create the command prompt
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.WorkingDirectory = @mysql;
+            cmd.Start();
+
+            //Enter the inputs
+            cmd.StandardInput.AutoFlush = true;
+            cmd.StandardInput.WriteLine("mysql -u root  " + (password != "" ? "-p " + password : "") + " < " + database);
+            cmd.StandardInput.WriteLine("mysql -u root " + (password != "" ? "-p " + password : "") + " < " + input + " > " + output);
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+
+            MessageBox.Show("The file has successfully been created: \n" +
+                "File Location: " + output,
+                "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        /// <summary>
+        /// Open a file explorer to find the .sql file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectInput_Click(object sender, EventArgs e)
+        {
+            DialogResult result = ofdInput.ShowDialog();
+
+            string file = ofdInput.FileName;
+            if (validateSQLFile(file))
+                txtInput.Text = file;
+
+        }
+
+        /// <summary>
+        /// Set the output text to equal the input text
+        /// Replaces .sql with .out
+        /// </summary>
+        private void updateOutputText()
+        {
+            txtOutput.Text = txtInput.Text.Replace(".sql", ".out");
+        }
+
+        /// <summary>
+        /// Open a file explorer to find the database .sql file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectDatabase_Click(object sender, EventArgs e)
+        {
+            DialogResult result = ofdInput.ShowDialog();
+
+            string file = ofdInput.FileName;
+            if (validateSQLFile(file))
+                txtDatabase.Text = file;
+
+        }
+
+        /// <summary>
+        /// Toggles the password char on/off
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = (txtPassword.PasswordChar == '\0' ? '•' : '\0');
+        }
+
+        /// <summary>
+        /// Checks if the selected file fits the requirements
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private bool validateSQLFile(string file)
+        {
+            bool valid = false;
+            if (file != null && !file.EndsWith(".sql"))
+                MessageBox.Show("Invalid .SQL file.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (file != null && file.Contains(" "))
+                MessageBox.Show("File path shouldn't contain a space.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                valid = true;
+            return valid;
+        }
+
+        /// <summary>
+        /// Update the output text when the input text gets changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            updateOutputText();
+        }
+    }
+}
